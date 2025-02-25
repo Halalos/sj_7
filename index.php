f0d7136732f7b8cc39c8896e48f58c1a53b4d132
Halalos <balogh.laszlo.andras@gmail.com>
2025-02-25 13:52:55 +0100

    Prva verzia

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hlavný nadpis</h1>
+    <?php
+    $meno = "Peter";
+    //Vypis cez echo
+    echo $meno;
+    echo "<br>"; 
+    //Vrati hodnotu datoveho typu a dlzku
+    var_dump($meno);
+
+    $farby =array("červená", "modrá", "zelená");
+    echo $farby[0];
+    echo ("<br>");
+
+    foreach($farby as $farba){
+        echo "Farba: ". $farba."<br>";
+    }
+    $data = array("Peter" => 25,
+                "Jana"=>30,
+                "Marek"=>20
+    );
+    foreach($data as $name => $age){
+        echo "$name ma $age rokov. <br>";  
+    }
+    ?>
+</body>
+</html>
